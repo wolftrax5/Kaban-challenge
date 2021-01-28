c282eb23ddb0b500d08a0d4546c3adee29fd35e3
Alejandro Medina <5almero5@gmail.com>
2021-01-27 22:33:44 -0600

    format code

--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -1,56 +1,60 @@
 import React, { useState } from "react";
 import "./index.css";
-import {getTasksByStage, STAGES_NAMES} from '../../utils/tasks'
+import { getTasksByStage, STAGES_NAMES } from "../../utils/tasks";
 
-
-const  KanbanBoard  = () => {
+const KanbanBoard = () => {
   const [tasks, setTasks] = useState([
-    { name: '1', stage: 0 },
-    { name: '2', stage: 0 },
-  ])
+    { name: "1", stage: 0 },
+    { name: "2", stage: 0 },
+  ]);
   // new task name
-  const [taskName, setTaskName] = useState('')
+  const [taskName, setTaskName] = useState("");
 
   const handlerInputName = (e) => {
-    setTaskName(e.target.value)
-    setError('')
-  }
-  const [error, setError] = useState('')
+    setTaskName(e.target.value);
+    setError("");
+  };
+  const [error, setError] = useState("");
   // Add task to Array
   const handlerNewTask = (name) => {
-    if(!name.trim()){
-      setError('Pls Type the name of Taks')
-      return
+    if (!name.trim()) {
+      setError("Pls Type the name of Taks");
+      return;
     }
 
-    if(tasks.some(task =>task.name === name)){
-      setError('That task already exists')
-      return
+    if (tasks.some((task) => task.name === name)) {
+      setError("That task already exists");
+      return;
     }
-    setTasks((state) => [{name, stage: 0} , ...state])
-    setTaskName('')
-    setError('')
-  }
+    setTasks((state) => [{ name, stage: 0 }, ...state]);
+    setTaskName("");
+    setError("");
+  };
   // mode task up
   const handleNextStage = (name, direction) => {
-      let taskindex = tasks.findIndex((task) => {
-          return task.name === name
-      })
-      if(taskindex < 0){
-        return
-      }
-      let task = tasks[taskindex];
-      const newStageIndex = direction === 'left' ? task.stage - 1 : task.stage + 1
-      if(newStageIndex >= STAGES_NAMES.length || newStageIndex < 0){
-        return
-      }
-      setTasks((state) => [ ...tasks.slice(0, taskindex), {...task, stage: newStageIndex}, ...tasks.slice(taskindex + 1)])
-  }
+    let taskindex = tasks.findIndex((task) => {
+      return task.name === name;
+    });
+    if (taskindex < 0) {
+      return;
+    }
+    let task = tasks[taskindex];
+    const newStageIndex =
+      direction === "left" ? task.stage - 1 : task.stage + 1;
+    if (newStageIndex >= STAGES_NAMES.length || newStageIndex < 0) {
+      return;
+    }
+    setTasks((state) => [
+      ...tasks.slice(0, taskindex),
+      { ...task, stage: newStageIndex },
+      ...tasks.slice(taskindex + 1),
+    ]);
+  };
   // Remove Task
   const handlerRemoveTask = (name) => {
-    let newTasks = tasks.filter((task) => task.name !== name)
-    setTasks((state) => [...newTasks])
-  }
+    let newTasks = tasks.filter((task) => task.name !== name);
+    setTasks((state) => [...newTasks]);
+  };
 
   let stagesTasks = getTasksByStage(tasks);
 
@@ -58,58 +62,89 @@ const  KanbanBoard  = () => {
     <div className="mt-20 layout-column justify-content-center align-items-center">
       <section className="mt-50 layout-row align-items-center justify-content-center">
         <div>
-          <input 
-          id="create-task-input" 
-          onChange={handlerInputName}
-          value={taskName}
-          type="text" 
-          className="large" 
-          placeholder="New task name" 
-          data-testid="create-task-input"/>
-          <span className='input_error'>{error}</span>
+          <input
+            id="create-task-input"
+            onChange={handlerInputName}
+            value={taskName}
+            type="text"
+            className="large"
+            placeholder="New task name"
+            data-testid="create-task-input"
+          />
+          <span className="input_error">{error}</span>
         </div>
-        <button 
-        disabled={!taskName.trim()}
-        type="submit" 
-        className="ml-30" 
-        data-testid="create-task-button" 
-        onClick={() => handlerNewTask(taskName)}>Create task</button>
+        <button
+          disabled={!taskName.trim()}
+          type="submit"
+          className="ml-30"
+          data-testid="create-task-button"
+          onClick={() => handlerNewTask(taskName)}
+        >
+          Create task
+        </button>
       </section>
 
       <div className="mt-50 layout-row">
-          {stagesTasks.map((tasks, i) => {
-              return (
-                  <div className="card outlined ml-20 mt-0" key={`${i}`}>
-                      <div className="card-text">
-                          <h4>{STAGES_NAMES[i]}</h4>
-                          <ul className="styled mt-50" data-testid={`stage-${i}`}>
-                              {tasks.map((task, index) => {
-                                  return <li className="slide-up-fade-in" key={`${i}${index}`}>
-                                    <div className="li-content layout-row justify-content-between align-items-center">
-                                      <span data-testid={`${task.name.split(' ').join('-')}-name`}>{task.name}</span>
-                                      <div className="icons">
-                                        <button disabled={task.stage === 0} onClick={()=> handleNextStage(task.name, 'left')} className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`}>
-                                          <i className="material-icons">arrow_back</i>
-                                        </button>
-                                        <button disabled={task.stage === 3} onClick={() => handleNextStage(task.name)} className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`}>
-                                          <i className="material-icons">arrow_forward</i>
-                                        </button>
-                                        <button onClick={()=> handlerRemoveTask(task.name)} className="icon-only danger x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-delete`}>
-                                          <i className="material-icons">delete</i>
-                                        </button>
-                                      </div>
-                                    </div>
-                                  </li>
-                              })}
-                          </ul>
-                      </div>
-                  </div>
-              )
-          })}
+        {stagesTasks.map((tasks, i) => {
+          return (
+            <div className="card outlined ml-20 mt-0" key={`${i}`}>
+              <div className="card-text">
+                <h4>{STAGES_NAMES[i]}</h4>
+                <ul className="styled mt-50" data-testid={`stage-${i}`}>
+                  {tasks.map((task, index) => {
+                    return (
+                      <li className="slide-up-fade-in" key={`${i}${index}`}>
+                        <div className="li-content layout-row justify-content-between align-items-center">
+                          <span
+                            data-testid={`${task.name
+                              .split(" ")
+                              .join("-")}-name`}
+                          >
+                            {task.name}
+                          </span>
+                          <div className="icons">
+                            <button
+                              disabled={task.stage === 0}
+                              onClick={() => handleNextStage(task.name, "left")}
+                              className="icon-only x-small mx-2"
+                              data-testid={`${task.name
+                                .split(" ")
+                                .join("-")}-back`}
+                            >
+                              <i className="material-icons">arrow_back</i>
+                            </button>
+                            <button
+                              disabled={task.stage === 3}
+                              onClick={() => handleNextStage(task.name)}
+                              className="icon-only x-small mx-2"
+                              data-testid={`${task.name
+                                .split(" ")
+                                .join("-")}-forward`}
+                            >
+                              <i className="material-icons">arrow_forward</i>
+                            </button>
+                            <button
+                              onClick={() => handlerRemoveTask(task.name)}
+                              className="icon-only danger x-small mx-2"
+                              data-testid={`${task.name
+                                .split(" ")
+                                .join("-")}-delete`}
+                            >
+                              <i className="material-icons">delete</i>
+                            </button>
+                          </div>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
+};
 
-}
-
-export default KanbanBoard
+export default KanbanBoard;
