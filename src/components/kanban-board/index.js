ad74afd1bad07264254f56f71be080886445bbd9
Alejandro Medina <5almero5@gmail.com>
2021-01-27 22:14:28 -0600

    Add error validations

--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -8,21 +8,28 @@ const  KanbanBoard  = () => {
     { name: '1', stage: 0 },
     { name: '2', stage: 0 },
   ])
-
   // new task name
   const [taskName, setTaskName] = useState('')
 
+  const handlerInputName = (e) => {
+    setTaskName(e.target.value)
+    setError('')
+  }
+  const [error, setError] = useState('')
   // Add task to Array
   const handlerNewTask = (name) => {
     if(!name.trim()){
+      setError('Pls Type the name of Taks')
       return
     }
 
     if(tasks.some(task =>task.name === name)){
+      setError('That task already exists')
       return
     }
     setTasks((state) => [{name, stage: 0} , ...state])
     setTaskName('')
+    setError('')
   }
   // mode task up
   const handlerNextStage = (name) => {
@@ -65,17 +72,22 @@ const  KanbanBoard  = () => {
   return (
     <div className="mt-20 layout-column justify-content-center align-items-center">
       <section className="mt-50 layout-row align-items-center justify-content-center">
-        
-        <input 
-        id="create-task-input" 
-        onChange={(e)=> setTaskName(e.target.value)}
-        value={taskName}
-        type="text" 
-        className="large" 
-        placeholder="New task name" 
-        data-testid="create-task-input"/>
-
-        <button type="submit" className="ml-30" data-testid="create-task-button" 
+        <div>
+          <input 
+          id="create-task-input" 
+          onChange={handlerInputName}
+          value={taskName}
+          type="text" 
+          className="large" 
+          placeholder="New task name" 
+          data-testid="create-task-input"/>
+          <span className='input_error'>{error}</span>
+        </div>
+        <button 
+        disabled={!taskName.trim()}
+        type="submit" 
+        className="ml-30" 
+        data-testid="create-task-button" 
         onClick={() => handlerNewTask(taskName)}>Create task</button>
       </section>
 
